d8a60494294ba48f8e0f39e93e072ccac23aae37
Егор <157805561+Pomidorkaeg@users.noreply.github.com>
2025-02-01 20:17:24 +0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,21 +1,14 @@
-document.addEventListener('DOMContentLoaded', () => {
-    // Запуск анимации капли крови при загрузке страницы
-    const bloodSpans = document.querySelectorAll('.blood');
-    bloodSpans.forEach(bloodSpan => {
-        setTimeout(() => {
-            bloodSpan.style.animation = 'blood-drip 1s ease-out forwards';
-        }, Math.random() * 2000); // Добавляем случайную задержку до 2 секунд
-    });
+// Отключаем правый клик
+document.addEventListener('contextmenu', function (e) {
+    e.preventDefault(); // Останавливаем контекстное меню
 });
 
-document.addEventListener('scroll', () => {
-    const sections = document.querySelectorAll('.section');
-    sections.forEach(section => {
-        const rect = section.getBoundingClientRect();
-        if (rect.top >= 0 && rect.bottom <= window.innerHeight) {
-            section.style.opacity = '1'; // Когда секция в области видимости, делаем её видимой
-        } else {
-            section.style.opacity = '0'; // Когда секция не в области видимости, скрываем её
-        }
-    });
+// Отключаем возможность копирования
+document.addEventListener('copy', function (e) {
+    e.preventDefault(); // Останавливаем копирование
+});
+
+// Запрещаем выделение текста
+document.addEventListener('selectstart', function (e) {
+    e.preventDefault(); // Останавливаем выделение текста
 });
