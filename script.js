d6e171c2d68e776294d436b38815a8b4c7d663c9
Егор <157805561+Pomidorkaeg@users.noreply.github.com>
2025-02-02 22:55:39 +0300

    Add files via upload

--- script.js
+++ script.js
@@ -12,3 +12,11 @@ document.addEventListener('copy', function (e) {
 document.addEventListener('selectstart', function (e) {
     e.preventDefault(); // Останавливаем выделение текста
 });
+
+document.getElementById('downloadButton').addEventListener('click', function() {
+    const url = 'https://github.com/Pomidorkaeg/luxw/blob/main/LuxWar.rar'; // ссылка на файл
+    const link = document.createElement('a'); // создаем элемент <a>
+    link.href = url; // ссылка на файл
+    link.download = 'LuxWar.rar'; // задаем имя файла для загрузки
+    link.click(); // инициируем клик по ссылке для загрузки
+});
